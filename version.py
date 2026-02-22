c7810540015f8f471bcf54328f295df1e95202cc
dsjchd <pawan246g@gmail.com>
2026-02-22 12:43:26 +0530

    v2

--- version.py
+++ version.py
@@ -1,4 +1,12 @@
-Import("env")
+# version.py
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+    env: Any
+
+Import("env")  # type: ignore
 
 with open("version.txt") as f:
     version = f.read().strip()
